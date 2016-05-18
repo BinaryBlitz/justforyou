54a46f20c91fb87939c27c9bc20823a6387ae23a
SumLare <mrhard95@gmail.com>
2016-05-18 20:47:18 +0300

    User model and controller tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:valid_one)
+  end
+
+  test 'valid' do
+    assert @user.valid?
+  end
+
+  test 'invalid without first name' do
+    @user.first_name = nil
+    assert @user.invalid?
+  end
+
+  test 'invalid without last name' do
+    @user.last_name = nil
+    assert @user.invalid?
+  end
+
+end
