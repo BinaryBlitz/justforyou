2e9a68b6219a5ceb27aa436a2f087174ac562dda
SumLare <mrhard95@gmail.com>
2016-05-19 01:59:35 +0300

    Admin model + devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json} do
+  devise_for :admins, path: 'admin', skip: :registrations
+  namespace :api do
     resources :users, only: [:index, :show]
   end
 end
