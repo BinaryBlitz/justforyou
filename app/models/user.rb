043b2056961d53148dd23dbeefc2852171aa9d54
Dan K <itsdanya@gmail.com>
2016-05-19 15:45:17 +0400

    Revert "User"

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  validates :first_name, :last_name, presence: true, length: { maximum: 20 }
-  validates :phone_number, format: { with: /\A[+]\d[0-9]{0,14}\z/}
-end
--- app/views/api/users/_user.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user,
-              :id, :first_name, :last_name, :email, :phone_number
