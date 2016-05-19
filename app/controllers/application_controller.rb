a08a71cf2eeec47d1d0a1d10e512c23825c3c5a3
SumLare <mrhard95@gmail.com>
2016-05-19 16:23:03 +0300

    Devise redirect logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     admin_users_path
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path
+  end
 end
