92c08fa48445d3d6e47e7dd0776650d55f4d0bd2
SumLare <mrhard95@gmail.com>
2016-05-21 19:17:45 +0300

    Merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins, path: 'admin', skip: :registrations
   namespace :api do
-<<<<<<< HEAD
-    resources :users, only: [:index, :show, :update, :destroy]
-  end
-  namespace :admin do
-    resources :users, except: [:new, :create]
-=======
     resources :users, only: [:index, :show]
   end
   namespace :admin do
     resources :users, only: [:index, :show]
->>>>>>> master
   end
 end
