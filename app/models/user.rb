f0f558aad108396ceec592f7b4dc7c564d3c8990
SumLare <mrhard95@gmail.com>
2016-05-21 21:11:18 +0300

    User full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true, length: { maximum: 20 }
   include Phonable
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
