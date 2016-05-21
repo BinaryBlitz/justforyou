76718cbe5c849a01792c5dda4be5ba046ffa8964
SumLare <mrhard95@gmail.com>
2016-05-21 23:28:16 +0300

    Programs admin panel crud

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518181819) do
+ActiveRecord::Schema.define(version: 20160521193052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20160518181819) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "programs", force: :cascade do |t|
+    t.string   "name",         null: false
+    t.text     "description",  null: false
+    t.integer  "duration",     null: false
+    t.integer  "first_price",  null: false
+    t.integer  "second_price", null: false
+    t.string   "preview_img",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
