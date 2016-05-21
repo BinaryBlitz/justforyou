8207f1d0f664d8a9f259bba23f6f8d86568b2758
SumLare <mrhard95@gmail.com>
2016-05-21 15:05:53 +0300

    Edit and delete for users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,33 @@
 class Api::UsersController < ApplicationController
+  before_action :find_user, only: [:show, :update, :destroy]
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      render json: @user, status: 200, location: [:api, @user]
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    @dream.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])    
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins, path: 'admin', skip: :registrations
   namespace :api do
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index, :show, :update, :destroy]
   end
   namespace :admin do
-    resources :users, only: [:index, :show]
+    resources :users, except: [:new, :create]
   end
 end
