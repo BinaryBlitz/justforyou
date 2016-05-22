d528d3d94ac1ef153fc8395d4aeeb4e69e9fa7c8
SumLare <mrhard95@gmail.com>
2016-05-22 13:38:38 +0300

    Admin panel views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for
+  def after_sign_in_path_for(resource)
     admin_users_path
   end
 
-  def after_sign_out_path_for
+  def after_sign_out_path_for(resource_or_scope)
     new_admin_session_path
   end
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -2,5 +2,6 @@ class Program < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :preview_img, :description, presence: true
   validates :duration, numericality: { greater_than: 0 }
-  validates :first_price, :second_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :first_price,
+            :second_price, numericality: { greater_than_or_equal_to: 0 }
 end
