24a716f12c59a85d3556b6f0b2728b972e2455f2
SumLare <mrhard95@gmail.com>
2016-05-23 23:54:45 +0300

    Users (#8)
    * Admin views
    
    * API: users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -14,4 +14,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get api_users_path(@user)
     assert_response :success
   end
+
+  test 'should update user' do
+    new_name = 'Another name'
+    patch api_user_path(@user), params: { user: { first_name: new_name } }
+    assert_response :ok
+    assert new_name, @user.reload.first_name
+  end
+
+  test 'should destroy user' do
+    assert_difference 'User.count', -1 do
+      delete api_user_url(@user)
+    end
+
+    assert_response :no_content
+  end
 end
