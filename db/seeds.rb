fdc78d5ca51218a0c27b07abb91e8525eaf1b0a1
SumLare <mrhard95@gmail.com>
2016-05-28 15:01:20 +0300

    Seeds fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 user = User.create!(
   first_name: 'Foo', last_name: 'Bar', phone_number: '+79998887766',
-  email: 'email@email.com';
+  email: 'email@email.com'
 )
 
 admin = Admin.create!(email: 'foo@bar.com', password: 'qwerty123')
