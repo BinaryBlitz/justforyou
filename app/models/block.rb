12cc99d2e455891269fe81cfd87fd66f14d2f133
Dan K <itsdanya@gmail.com>
2016-05-31 14:30:44 +0300

    Blocks: rename title to name

--- app/models/block.rb
+++ app/models/block.rb
@@ -3,7 +3,7 @@
 # Table name: blocks
 #
 #  id         :integer          not null, primary key
-#  title      :string           not null
+#  name       :string           not null
 #  image      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
---
+++ db/migrate/20160531112746_rename_title_to_name_in_blocks.rb
@@ -0,0 +1,5 @@
+class RenameTitleToNameInBlocks < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :blocks, :title, :name
+  end
+end
