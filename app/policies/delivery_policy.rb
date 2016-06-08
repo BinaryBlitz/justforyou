754173a76948b75203f08d73cbf5cb4d79366127
SumLare <mrhard95@gmail.com>
2016-06-08 16:44:22 +0300

    Set up Pundit (#28)

---
+++ app/policies/delivery_policy.rb
@@ -0,0 +1,11 @@
+class DeliveryPolicy < ApplicationPolicy
+  attr_reader :delivery
+
+  def initialize(delivery)
+    @delivery = delivery
+  end
+
+  def cancel?
+    ((delivery.scheduled_for - Time.zone.now) / 1.hour).round > 36
+  end
+end
