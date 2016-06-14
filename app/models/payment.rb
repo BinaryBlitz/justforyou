840c93682ef4d38402afd234e19e4baefa34147e
Dan K <itsdanya@gmail.com>
2016-06-15 02:44:49 +0300

    Payonline integration

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -13,8 +13,43 @@
 #
 
 class Payment < ApplicationRecord
+  CURRENCY = 'RUB'
+
+  before_validation :set_amount
+  after_create :pay, if: :payment_card
+
   belongs_to :order
   belongs_to :payment_card, optional: true
 
   validates :amount, numericality: { greater_than: 0 }
+
+  def payment_url
+    return if payment_card
+    Payonline::PaymentGateway.new(payment_options).payment_url
+  end
+
+  private
+
+  def set_amount
+    self.amount = order.total_price
+  end
+
+  def pay
+    logger.debug("Payment #{id}: refill")
+
+    # TODO: Make request
+    Payonline::RebillGateway.new(rebill_options).payment_url
+  end
+
+  def payment_options
+    {
+      order_id: order_id,
+      amount: amount,
+      currency: CURRENCY
+    }
+  end
+
+  def rebill_options
+    payment_options.merge(payment_card.rebill_anchor)
+  end
 end
