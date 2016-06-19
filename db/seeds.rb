12d83c3dfa58fec739dd2e5a3931db189d0a3da0
Dan K <itsdanya@gmail.com>
2016-06-19 11:30:58 +0300

    Add items and purchases to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,36 +11,38 @@ address = user.addresses.create(
   floor: 1
 )
 
-admin = Admin.create!(email: 'foo@bar.com', password: 'qwerty123')
+Admin.create!(email: 'foo@bar.com', password: 'qwerty123')
 
 image = File.open(Rails.root.join('test', 'fixtures', 'files', 'blank.jpg'))
 
 block = Block.create!(name: 'Block', image: image)
 
 program = Program.create!(
-  name: 'Program',
-  description: 'Description',
-  preview: 'Preview',
-  prescription: ['Prescription'],
-  threshold: 10,
-  primary_price: 2,
-  secondary_price: 2,
-  block: block,
-  preview_image: image
+  name: 'Program', description: 'Description', preview: 'Preview', prescription: ['Prescription'],
+  threshold: 10, primary_price: 2, secondary_price: 2,
+  block: block, preview_image: image
 )
 
 day = program.days.create!(position: 1)
 
-order = Order.create!(
-  user: user,
-  phone_number: '+79998887766',
+day.items.create!(
+  content: 'Content',
+  starts_at: '12:00', ends_at: '13:00',
+  calories: 100, weight: 100
+)
+
+Order.create!(
+  user: user, phone_number: '+79998887766',
   line_items_attributes: [
     { program: program, number_of_days: 10 }
   ]
 )
 
-product_type = ProductType.create!(name: 'Product type')
+purchase = user.purchases.create!(program: program, number_of_days: 5)
 
+purchase.deliveries.create!(address: address, scheduled_for: 1.month.from_now)
+
+product_type = ProductType.create!(name: 'Product type')
 product_type.products.create!(name: 'Product')
 
-promotion = Promotion.create!(name: 'Promotion', description: 'Description')
+Promotion.create!(name: 'Promotion', description: 'Description')
