d3a13701f9bb1c7246f458ff49c127d944b27d7a
Dan K <itsdanya@gmail.com>
2016-07-03 21:41:33 +0300

    Add tests for exports

--- test/controllers/admin/exports_controller_test.rb
+++ test/controllers/admin/exports_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class Admin::ExportsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in_admin
+  end
+
+  test 'should get kitchen exports' do
+    get kitchen_admin_exports_path
+    assert_response :success
+  end
+
+  test 'should get courier exports' do
+    get courier_admin_exports_path
+    assert_response :success
+  end
+
+  test 'should get manager exports' do
+    get manager_admin_exports_path
+    assert_response :success
+  end
 end
