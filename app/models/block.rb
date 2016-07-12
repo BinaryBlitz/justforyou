afd0c21179dbde86a8fb95b9d1ece7f477d6d999
SumLare <mrhard95@gmail.com>
2016-07-12 18:29:14 +0300

    Update validates format in Block

--- app/models/block.rb
+++ app/models/block.rb
@@ -15,7 +15,7 @@ class Block < ApplicationRecord
   has_many :programs, dependent: :destroy
 
   validates :name, :image, presence: true
-  validates_format_of :color, with: /\A#(?:[0-9a-f]{3})(?:[0-9a-f]{3})?\z/i
+  validates :color, format: { with: /\A#(?:[0-9a-f]{3})(?:[0-9a-f]{3})?\z/i }
 
   mount_uploader :image, ImageUploader
 end
