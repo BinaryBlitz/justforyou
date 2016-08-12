767af8484a354b51098fb69990515f559d9c832f
Dan K <itsdanya@gmail.com>
2016-08-13 00:19:11 +0900

    Admin: export only valid deliveries

--- app/controllers/admin/exports_controller.rb
+++ app/controllers/admin/exports_controller.rb
@@ -1,16 +1,24 @@
 class Admin::ExportsController < Admin::AdminController
+  before_action :set_deliveries
+
   def kitchen
-    @export = KitchenExport.new(Delivery.all)
+    @export = KitchenExport.new(@deliveries)
     send_data(@export.to_csv, filename: @export.filename)
   end
 
   def courier
-    @export = CourierExport.new(Delivery.all)
+    @export = CourierExport.new(@deliveries)
     send_data(@export.to_csv, filename: @export.filename)
   end
 
   def manager
-    @export = ManagerExport.new(Delivery.all)
+    @export = ManagerExport.new(@deliveries)
     send_data(@export.to_csv, filename: @export.filename)
+  end
+
+  private
+
+  def set_deliveries
+    @deliveries = Delivery.valid
   end
 end
