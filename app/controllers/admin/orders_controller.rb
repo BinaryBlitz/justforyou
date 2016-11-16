5ddcc250a690adce597abf9c0c31abfbdbe9b7bd
SumLare <mrhard95@gmail.com>
2016-11-16 22:50:42 +0300

    Add index page to orders (#98), render only paid orders (#99)

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,8 @@
 class Admin::OrdersController < Admin::AdminController
+  def index
+    @orders = Order.paid.order(created_at: :desc).page(params[:page]).per(10)
+  end
+
   def show
     @order = Order.find(params[:id])
   end
