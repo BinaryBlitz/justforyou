3328fdbad8fbb85076ef516674bf2458bfde2aa8
SumLare <mrhard95@gmail.com>
2017-02-12 00:40:54 +0300

    Update deliveries test

--- test/controllers/api/deliveries_controller_test.rb
+++ test/controllers/api/deliveries_controller_test.rb
@@ -4,6 +4,7 @@ class API::DeliveriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @delivery = deliveries(:delivery)
     @new_delivery = deliveries(:new_delivery)
+    @invalid_delivery = deliveries(:invalid_delivery)
     @purchase = @delivery.purchase
   end
 
@@ -25,7 +26,7 @@ class API::DeliveriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should not cancel delivery' do
-    patch cancel_api_delivery_path(@delivery, api_token: api_token), params: {
+    patch cancel_api_delivery_path(@invalid_delivery, api_token: api_token), params: {
       delivery: { status: 'canceled' }
     }
     assert_response :forbidden
@@ -36,5 +37,15 @@ class API::DeliveriesControllerTest < ActionDispatch::IntegrationTest
       delivery: { status: 'canceled' }
     }
     assert_response :ok
+  end
+
+  test 'should not create delivery' do
+    assert_difference 'Delivery.count', 0 do
+      post api_purchase_deliveries_url(@purchase, api_token: api_token), params: {
+        deliveries: [@invalid_delivery.attributes]
+      }
+    end
+
+    assert_response 422
   end
 end
