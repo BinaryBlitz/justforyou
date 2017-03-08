749d8dca3c34b5017cfbbcb1b8a7ee022b858541
SumLare <mrhard95@gmail.com>
2017-03-08 21:17:04 +0300

    Admin: test orders index page with 0 orders

--- test/controllers/admin/orders_controller_test.rb
+++ test/controllers/admin/orders_controller_test.rb
@@ -11,6 +11,13 @@ class Admin::OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'should get index with 0 orders' do
+    Order.destroy_all
+
+    get admin_orders_path
+    assert_response :success
+  end
+
   test 'should get show' do
     get admin_order_path(@order)
     assert_response :success
